4a41f0b596c50500566b6e628d2abd9d6ea25785
Suellen <isuelima@gmail.com>
2022-07-13 23:50:35 -0300

    server.ts error heroku get

--- src/server.ts
+++ src/server.ts
@@ -6,9 +6,9 @@ dotenv.config();
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello Sertão!'});
-})
+// app.get('/', (request, response) => {
+//     return response.json({ message: 'Hello Sertão!'});
+// })
 
 app.listen(3333, () => {
     console.log('🚀 Server started on port 3333!');
