43fd216768ea97e5bdbdf1239060011c48ac4dc9
Suellen <isuelima@gmail.com>
2022-07-13 23:39:48 -0300

    typeorm and heroku configured

---
+++ src/server.ts
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import './database';
+
+dotenv.config();
+
+const app = express();
+
+app.get('/', (request, response) => {
+    return response.json({ message: 'Hello Sertão!'});
+})
+
+app.listen(3333, () => {
+    console.log('🚀 Server started on port 3333!');
+})
